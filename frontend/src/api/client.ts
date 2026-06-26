888e12052445155acb21f804311214b558de8eda
luislajitas-create <luislajitas@gmail.com>
2026-06-25 21:49:09 -0300

    Update client.ts

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -7,7 +7,6 @@ export const api = axios.create({ baseURL });
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
-    config.headers = config.headers || {};
     config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
